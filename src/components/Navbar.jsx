3bafe670f8c75fc3a09184794666a0e471bf0cf1
Himani <himani.baliyan00@gmail.com>
2026-06-26 16:47:47 +0530

    installed lucid-react-package and set up Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Bell, Settings, UserCircle } from "lucide-react";
+
 function Navbar() {
   return (
-    <>
-      <h1>Navbar</h1>
-    </>
+    <header className="navbar">
+      <div className="navbar-left">
+        <h1>Complaint Dashboard</h1>
+        <p>Monitor and resolve complaints efficiently.</p>
+      </div>
+
+      <div className="navbar-right">
+        <button className="icon-btn">
+          <Bell size={20} />
+          <span className="notification-badge"></span>
+        </button>
+
+        <button className="icon-btn">
+          <Settings size={20} />
+        </button>
+
+        <div className="profile">
+          <UserCircle size={28} className="profile-icon" />
+          <div className="profile-info">
+            <span className="profile-name">Admin</span>
+            <span className="profile-role">Super User</span>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
